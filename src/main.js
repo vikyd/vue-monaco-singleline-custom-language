4ddd73952dabd5160093138a2cb86a7b5df58318
vikyd <zwj_fhys@qq.com>
2021-02-19 15:05:49 +0800

    chore: gtag

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueGtag from 'vue-gtag'
+
+if (process.env.NODE_ENV === 'production') {
+  Vue.use(VueGtag, { config: { id: 'G-XT0QM7C5VK' } })
+}
 
 Vue.config.productionTip = false
 
-new Vue({
+const v = new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+v.$gtag.pageview({
+  page_path: 'home',
+})
+
